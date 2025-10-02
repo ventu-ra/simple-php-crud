c050d033590ca8e92e58d06f4a68c83acd8a1c4f
ventu-ra <ventura09.10.0@gmail.com>
2025-10-02 20:16:26 -0300

    feat(database): create student table and seed data
    * Create `student` table with fields for name, address, nationality, and marks.
    * Insert multiple student records with relevant details.
    * Update index.php to display student details with action buttons.
    * Enhance styling in style.css for better layout.

--- crud_db.sql
+++ crud_db.sql
@@ -1,8 +1,77 @@
 -- Active: 1759406026980@@172.17.0.2@3306@crud_db
-CREATE TABLE IF NOT EXISTS `people` (
+CREATE TABLE IF NOT EXISTS `student` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(100) NOT NULL,
     `address` VARCHAR(255) NOT NULL,
     `nationality` VARCHAR(100) NOT NULL,
+    `marks` INT NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 12;
+);
+
+INSERT INTO
+    `student` (
+        `name`,
+        `address`,
+        `nationality`,
+        `marks`
+    )
+VALUES (
+        'Ana Silva',
+        'Rua das Flores, 123',
+        'Brasileira',
+        85
+    ),
+    (
+        'Carlos Souza',
+        'Av. Paulista, 456',
+        'Brasileiro',
+        90
+    ),
+    (
+        'Maria Oliveira',
+        'Rua Verde, 789',
+        'Portuguesa',
+        78
+    ),
+    (
+        'João Pereira',
+        'Rua Azul, 321',
+        'Brasileiro',
+        88
+    ),
+    (
+        'Fernanda Lima',
+        'Av. Central, 654',
+        'Brasileira',
+        92
+    ),
+    (
+        'Pedro Santos',
+        'Rua Amarela, 987',
+        'Brasileiro',
+        80
+    ),
+    (
+        'Lucas Costa',
+        'Av. Norte, 159',
+        'Brasileiro',
+        75
+    ),
+    (
+        'Juliana Alves',
+        'Rua Sul, 753',
+        'Brasileira',
+        95
+    ),
+    (
+        'Rafael Martins',
+        'Av. Leste, 852',
+        'Brasileiro',
+        83
+    ),
+    (
+        'Beatriz Ramos',
+        'Rua Oeste, 951',
+        'Brasileira',
+        89
+    );
--- index.php
+++ index.php
@@ -9,6 +9,15 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+  <script type="text/javascript">
+
+    $(document).ready(function () {
+
+      $('[data-toggle="tooltip"]').tooltip();
+
+    });
+
+  </script>
   <link rel="stylesheet" href="style.css">
 </head>
 
@@ -18,8 +27,63 @@
       <div class="row">
         <div class="col-md-12">
           <div class="page-header clearfix">
-            <h2 class="">Students</h2>
+            <h2 class="pull-left">Students Details</h2>
+            <a href="create.php" class="btn btn-primary pull-right">Add New Student</a>
           </div>
+          <?php
+
+          // Include config
+          
+          require_once "config.php";
+
+          // Attempt select query execution
+          $sql = "select * from student";
+          if ($result = mysqli_query($conn, $sql)) {
+            if (mysqli_num_rows($result) > 0) {
+              echo "<table class=`table-bordered table-striped table-hover`>";
+              echo "<thead>";
+              echo "<tr>";
+              echo "<th>#</th>";
+              echo "<th>Name</th>";
+              echo "<th>Address</th>";
+              echo "<th>Marks</th>";
+              echo "<th>Action</th>";
+              echo "</tr>";
+              echo "</thead>";
+              echo "<tbody>";
+              while ($row = mysqli_fetch_array($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['address'] . "</td>";
+                echo "<td>" . $row['marks'] . "</td>";
+                echo "<td>";
+                echo "<a href='read.php?id=" . $row['id'] . "' title='View Record' data-toggle='tooltip'><span class ='glyphicon glyphicon-eye-open'></span></a>";
+
+                echo "<a href='update.php?id=" . $row['id'] . "' title='Update Record' data-toggle='tooltip'><span class ='glyphicon glyphicon-pencil'></span></a>";
+
+                echo "<a href='delete.php?id=" . $row['id'] . "' title='Delete Record' data-toggle='tooltip'><span class ='glyphicon glyphicon-trash'></span></a>";
+                echo "</td>";
+                echo "</tr>";
+              }
+              echo "</tbody>";
+              echo "</table>";
+
+              // Free result set
+              mysqli_free_result($result);
+            } else {
+              echo "<p class='lead'><em>No records were found.</em></p>";
+            }
+          } else {
+            echo "Error: Could not able to execute $sql. " . mysqli_error($conn);
+          }
+          // Close connection
+          mysqli_close($conn);
+          ?>
+          <form method="post" action="search1.php?go" id="searchform">
+            <input type="text" name="name">
+            <input type="submit" name="submit" value="Search">
+          </form>
         </div>
       </div>
     </div>
