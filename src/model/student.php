ba4c56dd98fce05ecf5b9bbf56823b3ddf828630
ventu-ra <ventura09.10.0@gmail.com>
2025-10-04 09:39:38 -0300

    feat(student): make id property readonly in constructor

--- src/model/student.php
+++ src/model/student.php
@@ -2,7 +2,7 @@
 class Student
 {
   public function __construct(
-    public int $id = 0,
+    public readonly int $id = 0,
     public string $name = "",
     public string $address = "",
     public int|string $marks = ""
