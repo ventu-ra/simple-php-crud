6c298ed78ad83caddef193921c65cb292d196579
ventu-ra <ventura09.10.0@gmail.com>
2025-10-04 09:31:53 -0300

    feat(student): update constructor to set default values
    * Set default values for id, name, address, and marks
    * Allow marks to accept both int and string types

--- src/model/student.php
+++ src/model/student.php
@@ -2,12 +2,13 @@
 class Student
 {
   public function __construct(
-    public int $id,
-    public string $name,
-    public string $address,
-    public int $marks
+    public int $id = 0,
+    public string $name = "",
+    public string $address = "",
+    public int|string $marks = ""
   ) {
   }
+
 }
 ?>
 
